74102abac65f7d828af0f035e3779d93e18fdeb6
loloar123 <113786263+loloar123@users.noreply.github.com>
2023-06-11 11:40:50 +0300

    Add files via upload

--- db/mongoConnect.js
+++ db/mongoConnect.js
@@ -8,8 +8,8 @@ async function main() {
   mongoose.set("strictQuery", false);
   // וזה לווינדוס 11
   await mongoose.connect(
-    // `mongodb+srv://${config.db_user}:${config.db_password}@cluster0.v3l6j1t.mongodb.net/atid22`
-    `mongodb+srv://${config.db_user}:${config.db_password}@drinkorderparty.mxpvewi.mongodb.net/final`
+    `mongodb+srv://${config.db_user}:${config.db_password}@cluster0.v3l6j1t.mongodb.net/atid22`
+    // `mongodb+srv://${config.db_user}:${config.db_password}@drinkorderparty.mxpvewi.mongodb.net/final`
   );
   console.log("mongo connect atid22 local");
   // use `await mongoose.connect('mongodb://user:password@localhost:27017/test');` if your database has auth enabled
